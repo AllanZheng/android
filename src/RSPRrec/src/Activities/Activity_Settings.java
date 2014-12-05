6a1eb5b021bc10c95cb2f58fcfb29239cdfb8431
Aled Davies <aleddavy@gmail.com>
2014-12-05 18:39:48 +0000

    Outline Activity_Settings
    Using Class Diagram from design specification, mainly to remind myself
    what to include.

--- src/RSPRrec/src/Activities/Activity_Settings.java
+++ src/RSPRrec/src/Activities/Activity_Settings.java
@@ -1,7 +1,22 @@
-package activities;
+package Activities;
 
 import android.app.Activity;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.SeekBar;
+import utilities.Settings;
 
-public class Activity_Settings extends Activity {
+public class Activity_Settings extends Activity
+{
+	private SeekBar GPS;
+	private SeekBar sendPrompt;
+	private RadioButton WiFi;
+	private SeekBar savePhotoToDevice;
+	
+	public void getData(View v){};
+	
+	public boolean updateSessionSettings(){return true;};
+	
+	public void toMainMenu(View v){};
 
 }
