7b2ccd3f42807b89faa7fd1ae936b21a19f3ddad
rhc15 <rhc15@aber.ac.uk>
2015-01-21 10:28:14 +0000

    logout.php

---
+++ src/Web Prototype/Logout.php
@@ -0,0 +1,23 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+//remove all session variables
+session_unset();
+
+// destroy the session
+session_destroy();
+
+// redirects you back to the index 
+header('Location: index.php');
+exit;
+
+?>
+
+</body>
+</html>
+
